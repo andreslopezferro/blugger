61e43546219140dd11206c10f84586345a4c2d40
Andrés Felipe López Ferro <andreslopezferro@gmail.com>
2018-07-08 22:49:24 -0500

    Navbar and footer for blog.

---
+++ src/components/partials/FooterBlog.vue
@@ -0,0 +1,11 @@
+<template>
+  <footer class="container text-center bg-white py-3">
+    <a href="https://www.andreslopezferro.com" class="text-muted"><small>www.andreslopezferro.com</small></a>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
---
+++ src/components/partials/Navbar.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="bg-white sticky-top">
+    <header class="container text-center">
+      <h4 class="h2 font-weight-bold mb-0"><router-link to="/" class="text-dark">Blog</router-link></h4>
+      <small class="text-muted">andreslopezferro</small>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
