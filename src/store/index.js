3d5e8d135574048dc08ebbad7fcad50d6bb1754f
Andrés Felipe López Ferro <andreslopezferro@gmail.com>
2018-07-08 22:55:51 -0500

    Import posts' module

--- src/store/index.js
+++ src/store/index.js
@@ -2,8 +2,13 @@ import 'es6-promise/auto'
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+// Modules
+import postsModule from './modules/posts'
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-
+  modules: {
+    postsModule
+  }
 })
