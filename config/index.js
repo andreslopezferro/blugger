5633b9f5d0a174562477091497c0ac4c3d9f306f
Andrés Felipe López Ferro <andreslopezferro@gmail.com>
2018-07-08 22:58:28 -0500

    Config variables.

--- config/index.js
+++ config/index.js
@@ -40,7 +40,12 @@ module.exports = {
     // https://vue-loader.vuejs.org/en/options.html#cachebusting
     cacheBusting: true,
 
-    cssSourceMap: true
+    cssSourceMap: true,
+    // Blogger data.
+    blogId: '',
+    blogUrl: 'blog.com/',
+    apiKey: '',
+    bloggerApiUrl: 'https://www.googleapis.com/blogger/v3/blogs/'
   },
 
   build: {
@@ -72,6 +77,11 @@ module.exports = {
     // View the bundle analyzer report after build finishes:
     // `npm run build --report`
     // Set to `true` or `false` to always turn it on or off
-    bundleAnalyzerReport: process.env.npm_config_report
+    bundleAnalyzerReport: process.env.npm_config_report,
+    // Blogger data.
+    blogId: '',
+    blogUrl: 'blog.com',
+    apiKey: '',
+    bloggerApiUrl: 'https://www.googleapis.com/blogger/v3/blogs/'
   }
 }
