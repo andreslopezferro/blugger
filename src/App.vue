e1460733fe4ec34d1d3aed56ce0e0b2e9ebef7d7
Andrés Felipe López Ferro <andreslopezferro@gmail.com>
2018-07-08 22:55:24 -0500

    Layout.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
   <div id="app">
+    <navbar></navbar>
     <router-view/>
+    <footer-blog></footer-blog>
   </div>
 </template>
 
 <script>
+import Navbar from './components/partials/Navbar'
+import FooterBlog from './components/partials/FooterBlog'
 export default {
+  components: {Navbar, FooterBlog},
   name: 'App'
 }
 </script>
